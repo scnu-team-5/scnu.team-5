708a6e39426b602dae290074901639f9adb79290
hyukii2000 <90820985+hyukii2000@users.noreply.github.com>
2022-04-28 16:50:51 +0900

    Add files via upload

---
+++ eighth.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int a1,i=0,j=0,k=0;
+    int a[5][4]={
+        {a1,a2,a3,a4},
+        {a5,a6,a7,a8},
+        {a9,a10,a11,a12},
+        {0,0,0,0},
+        {0,0,0,0}
+    };
+    for(k=0; k<13;)
+    {
+        cin >> i;
+        cin >> j;
+        if( a[i][j]==0)
+        {
+            a[i][j]=1;
+            printf("loding ~ \n");
+            if(a[i][j]==1)printf("car in loding \n ========= \n The car has entered \n")i=j=0;
+            k++;
+        }
+        else if(k==12)printf("There's no space");
+        else
+        {
+            printf("error \n Put it on hold");
+        }
+        
+    }
+}
