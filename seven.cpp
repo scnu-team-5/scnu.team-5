02b80d4f21314de893301ad8e32581a5a16127f5
leejeonghoon12 <101316727+leejeonghoon12@users.noreply.github.com>
2022-04-28 16:48:02 +0900

    Add files via upload

---
+++ seven.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int a1, i = 0, j = 0, k = 0;
+    int a[5][4] = {
+        {0,0,0,0},
+        {0,0,0,0},
+        {0,0,0,0},
+        {0,0,0,0},
+        {0,0,0,0}
+    };
+    for (k = 0; k < 13;)
+    {
+        cin >> i;
+        cin >> j;
+        if (a[i][j] == 0)
+        {
+            a[i][j] = 1;
+            printf("loding ~ \n");
+            if (a[i][j] == 1)printf("car in loding \n ========= \n The car has entered \n")i = j = 0;
+            k +
